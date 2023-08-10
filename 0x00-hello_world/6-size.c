b099970846895709f5fbf6a84b0951899b080ba0
L8show <mizoelabd123@gmail.com>
2023-08-10 23:33:33 +0000

    cool

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio>
+
+/**
+ * main - Entery
+ *
+ * Description: using printof to print the size of various types by using sizof
+ *
+ * Return: Always 0 (success)
+ **/
+
+int main(void)
+{
+	printf("Size of a char: %ld byte(s)\n", sizeof(char));
+	printf("Size of a int: %ld byte(s)\n", sizeof(int));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %ld byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %ld byte(s)\n", sizeof(float));
+	return (0);
+}
+
