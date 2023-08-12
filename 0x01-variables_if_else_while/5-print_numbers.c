fc8b4c877f6384374593b13b3808a6213861e640
L8show <mizoelabd123@gmail.com>
2023-08-12 01:55:51 +0000

    cool

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entery point
+ *
+ * Description: Print alphabet in lowercase then uppercase
+ *
+ * Return: 0 (success)
+*/
+int main(void)
+{
+	int digit = 0;
+
+	while (digit <= 9)
+	{
+		printf("%d\n", digit);
+	digit++;
+}
+
+return (0);
+}
