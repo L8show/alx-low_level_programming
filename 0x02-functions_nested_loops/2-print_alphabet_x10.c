78142baeee20d5713b084257d82d80fc766a53ca
L8show <mizoelabd123@gmail.com>
2023-08-16 00:53:36 +0000

    cool

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include"main.h"
+
+/**
+ * print_alphabet_x10 - adasdadsadasdasdd 10 adads
+ *
+*/
+
+void print_alphabet_x10(void)
+{
+	int word, sword;
+
+	for (sword = 1; sword <= 10; sword++)
+	{
+	for (word = 'a' ; word <= 'z' ; word++)
+		_putchar(word);
+	_putchar('\n');
+	}
+}
