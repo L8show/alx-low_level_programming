38ae5ae4325fed415852244dcec2c87cc3bd1cc5
L8show <mizoelabd123@gmail.com>
2023-08-16 01:04:39 +0000

    cool

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include"main.h"
+
+/**
+ * _islower - adasdadsadasdasdd 10 adads
+ *
+ * @c: adasdasdasd
+ *
+ * Return: ae'lae 1 ad['aods 0 dads
+*/
+
+int _islower(int c)
+{
+if (c >= 97 && c <= 122)
+	return (1);
+else
+	return (0);
+}
