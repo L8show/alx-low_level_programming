0158701f5641f617b4503978f3f1d097202e203a
L8show <mizoelabd123@gmail.com>
2023-08-16 20:50:06 +0000

    cool

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,9 +12,9 @@ void print_to_98(int n)
 		for (count = n; count > 98; count--)
 			printf("%d, ", count);
 	else
-		for (count = n; count > 98; count++)
+		for (count = n; count < 98; count++)
 			printf("%d, ", count);
-				printf("98\n");
+	printf("98\n");
 }
 
 
