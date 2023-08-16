b3c56817890566b84ce33f9543e2b9734b5e9d05
L8show <mizoelabd123@gmail.com>
2023-08-16 16:49:57 +0000

    cool

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,8 +8,8 @@ void jack_bauer(void)
 {
 	int hr, min;
 
-	for (min = 0; min <= 59; min++)
-	{
+			for (min = 0; min <= 59; min++)
+			{
 			for (hr = 0; hr <= 23; hr++)
 			{
 				_putchar((hr / 10) + 48);
