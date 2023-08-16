56278b6235ddbf2c9e3473ce53dc9cb18cf8f9f4
L8show <mizoelabd123@gmail.com>
2023-08-16 01:30:46 +0000

    cool

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include"main.h"
+
+/**
+ * print_sign - adasdadsadasdasdd 10 adads
+ *
+ * @n: adasdasdasd
+ *
+ * Return: ae'lae 1 ad['aods 0 dads
+*/
+
+int print_sign(int n)
+{
+if (n > 0)
+{
+	_putchar(43);
+	return (1);
+}
+else if (n == 0)
+{
+	-putchar(48);
+	return (0);
+}
+else
+{
+	_putchar(45);
+	return (-1);
+}
+}
