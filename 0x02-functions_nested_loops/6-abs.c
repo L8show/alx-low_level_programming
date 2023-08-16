a14b9205794f3e59dbdd22831701827c4858841c
L8show <mizoelabd123@gmail.com>
2023-08-16 01:48:45 +0000

    cool

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include"main.h"
+
+/**
+ * _abs - adasdadsadasdasdd 10 adads
+ *
+ * @i: adasdasdasd
+ *
+ * Return: ae'lae 1 ad['aods 0 dads
+*/
+int _abs(int i)
+{
+	if (i < 0)
+		i = (-1) * i;
+	return (i);
+}
