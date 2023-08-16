5671273c434c25c5ab6f44258b831f12cdb59ff6
L8show <mizoelabd123@gmail.com>
2023-08-16 02:09:12 +0000

    cool

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include"main.h"
+
+/**
+ * print_last_digit - adasdadsadasdasdd 10 adads
+ *
+ * @n: adasdasdasd
+ *
+ * Return: ae'lae 1 ad['aods 0 dads
+*/
+int print_last_digit(int n)
+{
+int z = n % 10;
+_putchar(z + '0');
+return (z);
+}
+
