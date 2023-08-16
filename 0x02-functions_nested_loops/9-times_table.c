3172dc0755f29805c31033bb8ae8cfff7cd068a7
L8show <mizoelabd123@gmail.com>
2023-08-16 20:16:42 +0000

    cool

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,34 +1,35 @@
-#include"main.h"
+#include "main.h"
 
 /**
- * times_table - adasdadsadasdasdd 10 ajhkl
+ *  times_table - asdasjklklgfgjgg 9 ads
+ *
+ *  Example table
+ *  0, 0, 0, 0, ..
+ *  0, 1, 2, 3, ..
+ *
 */
 
 void times_table(void)
 {
 	int num, mult, prod;
 
-
 	for (num = 0; num <= 9; num++)
 	{
-
 		_putchar(48);
-		for (mult = 1; num <= 9; mult++)
+		for (mult = 1; mult <= 9; mult++)
 		{
 			_putchar(',');
-			_putchar(' ');
+			_putchar(',');
 
-			prod = mult * num;
+			prod = num * mult;
 
-		if (prod <= 9)
-			_putchar (' ');
-		else
-			_putchar((prod / 10) + 48);
+			if (prod <= 9)
+				_putchar(' ');
+			else
+				_putchar((prod / 10) + 48);
 
 			_putchar((prod % 10) + 48);
-			}
+		}
 		_putchar('\n');
 	}
-
 }
-
