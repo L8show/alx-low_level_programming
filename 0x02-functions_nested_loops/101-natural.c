b4ff2d1dd880a19e21309dfbe4ced9aaf5a8f811
L8show <mizoelabd123@gmail.com>
2023-08-16 02:59:26 +0000

    cool

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,23 @@
+#include"main.h"
+
+/**
+ * main - adasdadsadasdasdd 10 adads
+ *
+ * Description: adasdasdasd
+ *
+ * Return: ae'lae 1 ad['aods 0 dads
+*/
+
+int main(void)
+{
+	int sum, num;
+
+	for (num = 0; num < 1024; num++)
+	{
+		if ((num % 3 == 0) || (num % 5 == 0))
+			sum += num;
+	}
+	printf("%d\n", sum);
+
+	return (0);
+}
