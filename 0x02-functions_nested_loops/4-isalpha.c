bffe9dfaaa072b41abea334ec3ec95e3b2ac7be4
L8show <mizoelabd123@gmail.com>
2023-08-16 01:10:57 +0000

    cool

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include"main.h"
+
+/**
+ * _isalpha - adasdadsadasdasdd 10 adads
+ *
+ * @c: adasdasdasd
+ *
+ * Return: ae'lae 1 ad['aods 0 dads
+*/
+
+int _isalpha(int c)
+{
+if (c >= 97 && c <= 122)
+	return (1);
+if (c >= 65 && c <= 90)
+	return (1);
+else
+	return (0);
+}
