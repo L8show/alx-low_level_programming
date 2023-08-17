7976f0d8f35f086e67949c3c4022ac132e8a947d
L8show <mizoelabd123@gmail.com>
2023-08-17 16:45:42 +0000

    cool

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isdigit - sadasda dadsads
+ *
+ * @c: asdasdadads
+ *
+ * Return: asdasdadasd
+*/
+
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+	else
+		return (0);
+}
