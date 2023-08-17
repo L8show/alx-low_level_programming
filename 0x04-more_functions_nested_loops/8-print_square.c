f2e7e712b973483b063bef20b803d1b58e9d2511
L8show <mizoelabd123@gmail.com>
2023-08-17 23:58:38 +0000

    cool

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_square - adasdasdasd
+ *
+ * @size: asdasdadadsdas
+ *
+ * Return: aasdasdasdasasdsa
+*/
+void print_square(int size)
+{
+	int r, c;
+
+	for (r = 1; r <= size; r++)
+	{
+		for (c = 1; c <= size; c++)
+			_putchar('#');
+		_putchar('\n');
+	}
+}
