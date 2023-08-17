862a7192c3a3eac0d1bfb140ae41334e70d5e43c
L8show <mizoelabd123@gmail.com>
2023-08-17 16:55:07 +0000

    cool

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - sadasda dadsads
+ *
+ * @a: asdasdadad
+ *
+ * @b: asdasdasdad
+ *
+ * Return: asdasdadasd
+*/
+
+int mul(int a, int b)
+{
+	int mul = (a * b);
+
+	return (mul);
+}
