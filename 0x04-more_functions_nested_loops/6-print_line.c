7045d2688581bb86893157a429014b919b896211
L8show <mizoelabd123@gmail.com>
2023-08-17 20:41:22 +0000

    cool

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_line - asdasdasdasdadasd
+ *
+ * @n: asdasdasdasdasda
+*/
+
+void print_line(int n)
+{
+	int dc;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		for (dc = 1; dc >= n; dc++)
+			_putchar('_');
+	_putchar('\n');
+	}
+}
+
+
