e18b71c30a18805677664231ef38a3361e0a4625
L8show <mizoelabd123@gmail.com>
2023-08-17 17:01:15 +0000

    cool

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_numbers - sadasda dadsads
+*/
+
+void print_numbers(void)
+{
+	int a;
+
+	for (a = 0; a <= 9; a++)
+		_putchar(a);
+	_putchar('\n');
+
+
+}
