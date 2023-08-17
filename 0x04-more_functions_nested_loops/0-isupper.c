b9e3248ebccc49500bd5e4d9e23abc235821a8f6
L8show <mizoelabd123@gmail.com>
2023-08-17 16:09:06 +0000

    cool

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isupper - sadasda dadsads
+ *
+ * @c: asdasdadads
+ *
+ * Return: asdasdadasd
+*/
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+		return (1);
+	else
+		return (0);
+}
