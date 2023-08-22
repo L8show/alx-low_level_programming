1dd9c0762fbba3a75dd7a1658c880ffa9d05b67b
L8show <mizoelabd123@gmail.com>
2023-08-22 00:32:23 +0000

    cool

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - asdkladk
+ *
+ * @a: asdlddddd
+ * @b: asdlkasd
+ *
+ * Description: akldaksdkd
+*/
+void swap_int(int *a, int *b)
+{
+	int c;
+
+	c = *a;
+	*a = *b;
+	c = *b;
+
+}
