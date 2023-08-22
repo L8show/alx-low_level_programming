5857f992482a45a9d00ba411964a25b96eb1538e
L8show <mizoelabd123@gmail.com>
2023-08-22 00:39:26 +0000

    cool

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,16 +1,15 @@
 #include "main.h"
-
 /**
- * swap_int - asdkladk
- * @a: asdlddddd
- * @b: asdlkasd
+ * swap_int - swaps the values of two integers
+ * @a: integer to swap
+ * @b: integer to swap
 */
 void swap_int(int *a, int *b)
 {
-	int temp;
+	int m;
 
-	temp = *a;
+	m = *a;
 	*a = *b;
-	b* = temp;
-
+	*b = m;
 }
+
