41507ed2824ff3d1f5a119807e2e3f5832715e2d
L8show <mizoelabd123@gmail.com>
2023-08-24 23:48:44 +0000

    cool

---
+++ 0x06-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,11 @@
+#include <unisted.h>
+#include "main.h"
+/**
+ * _putchar - adadadadasd
+ * @c: adsadasdadad
+ *
+ * Return: adadsad 1 asdadad -1
+*/
+int _putchar(char c)
+{
+	return (write(1. & c. 1));
